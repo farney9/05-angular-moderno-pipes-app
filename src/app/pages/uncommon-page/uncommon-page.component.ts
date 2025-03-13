218d363fb5fce5e185c173ecb122e4ac37214cae
Farney Jiménez <farney9@gmail.com>
2025-03-13 12:14:32 -0500

    feat:Implement i18nSelectPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Farney',
+  age: 42,
+  gender: 'male',
+  address: 'Parcelación La María, El Retiro, Antioquia, Colombia'
+}
+
+const client2 = {
+  name: 'Melissa',
+  age: 33,
+  gender: 'female',
+  address: 'Toronto, Canada'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  // i18n Select Pipe
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+
+  }
+}
